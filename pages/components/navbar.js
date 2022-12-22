06518d6c7e23ebf8c8cbad1529da864e0e86fd2e
Shazeb Khan <shazebk.ug21.cs@nitp.ac.in>
2022-12-22 18:29:28 +0530

    Some minor changes done in Navbar

--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import LinkTo from "./navlink";
+import { burger, close, user, message } from "./icons";
 
 function Navbar() {
   const [display, setDisplay] = useState("hidden");
@@ -12,9 +13,7 @@ function Navbar() {
 
   return (
     <div className=" fixed w-full h-14 border-b px-4 flex items-center justify-between">
-      <Link href="/">
-        <h1 className="font-semibold text-xl md:text-base">TRIALSHOPPY</h1>
-      </Link>
+      <Link href="/"><h1 className="font-semibold text-lg md:text-base">TRIALSHOPPY</h1></Link>
 
       <button
         onClick={clickHandler}
@@ -24,41 +23,11 @@ function Navbar() {
         aria-controls="navbar-multi-level"
         aria-expanded="false"
       >
-        <span class="sr-only">Open main menu</span>
-        <svg
-          class="w-8 h-8"
-          aria-hidden="true"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-            clip-rule="evenodd"
-          ></path>
-        </svg>
+      {burger}
       </button>
 
-      <div
-        className={`h-screen w-3/5 p-4 bg-orange-300 fixed top-0 right-0 items-center justify-between ${display} md:h-full md:flex md:static md:w-1/2 md:bg-white`}
-      >
-        <div className="md:hidden" onClick={clickHandler}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-10 h-10"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
-        </div>
+      <div className={`h-screen w-3/5 p-4 bg-orange-300 fixed top-0 right-0 items-center justify-between ${display} md:h-full md:flex md:static md:w-1/2 md:bg-white`}>
+        <div className="md:hidden" onClick={clickHandler}>{close} </div>
         <LinkTo linkTo="employee" />
         <LinkTo linkTo="department" />
         <LinkTo linkTo="attendance" />
@@ -66,39 +35,10 @@ function Navbar() {
         <LinkTo linkTo="meeting" />
         <LinkTo linkTo="leave" />
       </div>
+
       <div className="w-20 justify-between hidden md:flex">
-        <Link href="/">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
-            />
-          </svg>
-        </Link>
-        <Link href="/">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-6 h-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-            />
-          </svg>
-        </Link>
+        <Link href="/">{user}</Link>
+        <Link href="/">{message}</Link>
       </div>
     </div>
   );
